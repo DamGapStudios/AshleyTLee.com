c3d7ef9c8a6b6888dd2adb9ed9f0d24f30e02f83
adunham95 <Apd@21295>
2017-04-13 10:50:45 -0400

    Created views folder

--- test/unit/Hello.spec.js
+++ test/unit/Hello.spec.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Hello from '../../src/components/Hello.vue'
+import Hello from '../../src/Views/Hello.vue'
 
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
