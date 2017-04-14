ca12db44ba821ce96da3fb9183ac454b656c9b86
Anthony <Gareraanthony@gmail.com>
2017-04-13 22:18:12 -0400

    Added all needed pages to the router for now.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/views/Hello'
 import Home from '@/views/Home'
+import About from '@/views/About'
+import Messages from '@/views/Messages'
+import App from '@/views/App'
+import Events from '@/views/Events'
+import Blog from '@/views/Blog'
+import Contact from '@/views/Contact'
 
 Vue.use(Router)
 
@@ -17,6 +23,36 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/messages',
+      name: 'Messages',
+      component: Messages
+    },
+    {
+      path: '/app',
+      name: 'App',
+      component: App
+    },
+    {
+      path: '/events',
+      name: 'Events',
+      component: Events
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/blog',
+      name: 'Blog',
+      component: Blog
     }
   ]
 })
---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="template">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'blog',
+    data () {
+      return {
+        title: 'Blog'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
