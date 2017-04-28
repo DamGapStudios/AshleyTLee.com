6cf5f2a884ca0fe5ada65e64d8664be79144e183
adunham95 <Apd@21295>
2017-04-28 17:00:26 -0400

    added grid to messages page

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -1,20 +1,81 @@
 <template>
-  <div id="template">
+  <div id="messages">
+    <home-canvas></home-canvas>
+
+    <div class="center-stuff">
+      <h2 class="ashley-blue-text">Messages</h2>
+      <p>Click on an audio teaching below to listen to free teaching of the bible</p>
+      <div class="audio-container">
+        <div class="audio-block" v-for="teaching in audioTeaching">
+          <div class="audio-image ashley-blue"></div>
+          {{ teaching.author }}<br/>
+          {{ teaching.date }}
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script>
+  import HomeCanvas from '../components/homeCanvas'
   export default {
-    name: 'blog',
+    components: {HomeCanvas},
+    name: 'messages',
     data () {
       return {
-        title: 'Blog'
+        title: 'messages',
+        audioTeaching: [
+          {
+            author: 'Daniel',
+            date: 'January 1 2017'
+          },
+          {
+            author: 'Jeremiah',
+            date: 'May 1 2016'
+          },
+          {
+            author: 'Isaiah',
+            date: 'June 27 2016'
+          },
+          {
+            author: 'Isaiah',
+            date: 'June 27 2016'
+          },
+          {
+            author: 'Daniel',
+            date: 'January 1 2017'
+          },
+          {
+            author: 'Jeremiah',
+            date: 'May 1 2016'
+          },
+          {
+            author: 'Isaiah',
+            date: 'June 27 2016'
+          },
+          {
+            author: 'Isaiah',
+            date: 'June 27 2016'
+          }
+        ]
       }
     }
   }
 </script>
 
 <style>
-
+  .audio-container {
+    display: grid;
+    grid-gap: 40px;
+    grid-template-columns: repeat(auto-fill, minmax(200px,1fr));
+  }
+  .audio-block{
+    /*padding: 20px;*/
+    font-size: 24pt;
+  }
+  .audio-image{
+    width: 200px;
+    height: 200px;
+  }
 </style>
