54c404ee666353c257cf716bd045a5f709dcbb27
adunham95 <adunham95@gmail.com>
2017-05-01 23:23:21 -0400

    working on canvas element

--- src/components/homeCanvas.vue
+++ src/components/homeCanvas.vue
@@ -1,6 +1,6 @@
 <template>
-  <div style="background-color: #5CD1D6" class="center-stuff ">
-    <canvas id="myCanvas" width="200" height="200">
+  <div style="background-color: #5CD1D6; display: flex; justify-content: center" class="center-stuff">
+    <canvas id="myCanvas" width="200" height="500">
     </canvas>
   </div>
 </template>
@@ -15,16 +15,15 @@
 //       create a rectangle object
       let rect = new fabric.Rect({
         left: 100,
-        top: 100,
-        fill: 'red',
-        width: 20,
-        height: 20,
-        angle: 45
+        top: 180,
+        fill: '#F0F0F0',
+        width: 100,
+        height: 40
       })
-
       canvas.add(rect)
       canvas.selection = false
       rect.set('selectable', false)
+      canvas.renderAll()
     },
     name: 'home-canvas',
     data () {
