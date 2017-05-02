44a15a87446c3e097e36bb2cc3fcbed98c4902d3
adunham95 <adunham95@gmail.com>
2017-05-02 00:54:18 -0400

    working on adding items to canvas Issue #1

--- src/components/homeCanvas.vue
+++ src/components/homeCanvas.vue
@@ -1,7 +1,14 @@
 <template>
   <div style="background-color: #5CD1D6; display: flex; justify-content: center" class="center-stuff">
-    <canvas id="myCanvas" width="200" height="500">
-    </canvas>
+    <canvas id="myCanvas" width="500" height="500"></canvas>
+    <img src="../assets/canvas_book.png" id="canvasBook"/>
+    <img src="../assets/canvas_calender.png" id="canvasCalender"/>
+    <img src="../assets/canvas_computer.png" id="canvasComputer"/>
+    <img src="../assets/canvas_holder.png" id="canvasHolder"/>
+    <img src="../assets/canvas_phone.png" id="canvasPhone"/>
+    <img src="../assets/canvas_portrait.png" id="canvasPortrait"/>
+    <img src="../assets/canvas_poster.png" id="canvasPoster"/>
+    <img src="../assets/canvas_vase.png" id="canvasVase"/>
   </div>
 </template>
 
@@ -11,19 +18,24 @@
   export default {
     mounted () {
       let canvas = new fabric.Canvas('myCanvas')
-
-//       create a rectangle object
-      let rect = new fabric.Rect({
+      let imgBook = document.getElementById('canvasBook')
+      let imgInstance = new fabric.Image(imgBook, {
         left: 100,
-        top: 180,
-        fill: '#F0F0F0',
-        width: 100,
-        height: 40
+        top: 100,
+        height: 50,
+        width: 60
       })
-      canvas.add(rect)
-      canvas.selection = false
-      rect.set('selectable', false)
-      canvas.renderAll()
+
+      fabric.Image.fromURL('canvas_phone.png', function (oImg) {
+        oImg.setLeft(0)
+        oImg.setTop(0)
+        canvas.add(oImg)
+// eslint-disable-next-line quotes
+        console.log('Added Image')
+        canvas.renderAll()
+      })
+
+      canvas.add(imgInstance)
     },
     name: 'home-canvas',
     data () {
@@ -34,6 +46,8 @@
   }
 </script>
 
-<style>
-
+<style scoped>
+img{
+  visibility: hidden;
+}
 </style>
