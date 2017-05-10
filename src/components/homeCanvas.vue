137f4eeb0795ee3853bb90b5e78d59c90e552930
adunham95 <Apd@21295>
2017-05-10 12:17:03 -0400

    Initial commit for canvas branch

--- src/components/homeCanvas.vue
+++ src/components/homeCanvas.vue
@@ -1,39 +1,31 @@
 <template>
   <div style="background-color: #5CD1D6; display: flex; justify-content: center" class="center-stuff">
     <canvas id="myCanvas" width="500" height="500"></canvas>
-    <!--<img src="../assets/canvas_book.png" id="canvasBook"/>-->
-    <!--<img src="../assets/canvas_calender.png" id="canvasCalender"/>-->
-    <!--<img src="../assets/canvas_computer.png" id="canvasComputer"/>-->
-    <!--<img src="../assets/canvas_holder.png" id="canvasHolder"/>-->
-    <!--<img src="../assets/canvas_phone.png" id="canvasPhone"/>-->
-    <!--<img src="../assets/canvas_portrait.png" id="canvasPortrait"/>-->
-    <!--<img src="../assets/canvas_poster.png" id="canvasPoster"/>-->
-    <!--<img src="../assets/canvas_vase.png" id="canvasVase"/>-->
   </div>
 </template>
 
 <script>
-  import fabric from 'fabric/dist/fabric'
+  import fabric from 'fabric/dist/fabric.require.js'
 
   export default {
     mounted () {
-      let canvas = new fabric.Canvas('myCanvas')
-      let imgBook = document.getElementById('canvasBook')
+      let canvas = new fabric.Canvas('myCanvas');
+      let imgBook = document.getElementById('canvasBook');
       let imgInstance = new fabric.Image(imgBook, {
         left: 100,
         top: 100,
         height: 50,
         width: 60
-      })
+      });
 
-      fabric.Image.fromURL('canvas_phone.png', function (oImg) {
-        oImg.setLeft(0)
-        oImg.setTop(0)
-        canvas.add(oImg)
+      fabric.Image.fromURL('wp-content/themes/AshleyTLeeTheme/dist/ashley_portrait.png', function (oImg) {
+        oImg.setLeft(0);
+        oImg.setTop(0);
+        canvas.add(oImg);
 // eslint-disable-next-line quotes
-        console.log('Added Image')
+        console.log('Added Image');
         canvas.renderAll()
-      })
+      });
 
       canvas.add(imgInstance)
     },
