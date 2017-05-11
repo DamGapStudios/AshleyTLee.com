d193a04e2a854307d1f332357bc40ecaac4b2be2
adunham95 <Apd@21295>
2017-05-10 21:19:53 -0400

    Working on blog post component

---
+++ src/components/blogpost.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        {{ post.title.rendered }}
+        <div v-html="post.content.rendered"></div>
+        Some Text here
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'blog-post',
+        prop: ['blogPost'],
+        computed: {
+        }
+    }
+</script>
+
+<style>
+
+
+</style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="blog">
+    <!--<blog-post v-for="post in blog_posts" :blog-post="post"></blog-post>-->
     <div v-for="post in blog_posts">
       {{ post.title.rendered }}
       <div v-html="post.content.rendered">
@@ -9,8 +10,10 @@
 </template>
 
 <script>
+    import BlogPost from "../components/blogpost.vue";
   export default {
-    created () {
+      components: {BlogPost},
+      created () {
       this.$http.get('http://ashleytlee.dev/wp-json/wp/v2/posts').then(response => {
         this.blog_posts = response.body
         console.log(this.blog_posts)
