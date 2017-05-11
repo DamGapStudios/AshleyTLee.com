4d791e96cd858ead1d14b7cbdedc02d5aece46e0
adunham95 <Apd@21295>
2017-05-10 23:05:29 -0400

    added all images to the canvas

--- src/components/homeCanvas.vue
+++ src/components/homeCanvas.vue
@@ -1,6 +1,6 @@
 <template>
-  <div style="background-color: #5CD1D6; display: flex; justify-content: center" class="center-stuff">
-    <canvas id="myCanvas" width="500" height="500"></canvas>
+  <div style="background-color: #A8DBDF; display: flex; justify-content: center" class="center-stuff">
+    <canvas id="myCanvas" width="1143px" height="450px"></canvas>
   </div>
 </template>
 
@@ -9,25 +9,90 @@
 
   export default {
     mounted () {
-      let canvas = new fabric.Canvas('myCanvas');
-      let imgBook = document.getElementById('canvasBook');
-      let imgInstance = new fabric.Image(imgBook, {
-        left: 100,
-        top: 100,
-        height: 50,
-        width: 60
-      });
+      let canvas = new fabric.StaticCanvas('myCanvas');
 
-      fabric.Image.fromURL('wp-content/themes/AshleyTLeeTheme/dist/ashley_portrait.png', function (oImg) {
-        oImg.setLeft(0);
-        oImg.setTop(0);
-        canvas.add(oImg);
+      fabric.Image.fromURL('wp-content/themes/AshleyTLeeTheme/dist/canvas_desk.png', function (desk) {
+        desk.setLeft(0);
+        desk.setTop(132);
+        canvas.add(desk);
 // eslint-disable-next-line quotes
-        console.log('Added Image');
+        console.log('Added Desk');
         canvas.renderAll()
       });
 
-      canvas.add(imgInstance)
+        fabric.Image.fromURL('wp-content/themes/AshleyTLeeTheme/dist/canvas_poster.png', function (poster) {
+            poster.setLeft(250);
+            poster.setTop(-80);
+            canvas.add(poster);
+// eslint-disable-next-line quotes
+            console.log('Added Poster');
+            canvas.renderAll()
+        });
+
+      fabric.Image.fromURL('wp-content/themes/AshleyTLeeTheme/dist/canvas_computer.png', function (computer) {
+        computer.setLeft(350);
+        computer.setTop(120);
+        canvas.add(computer);
+// eslint-disable-next-line quotes
+        console.log('Added Computer');
+        canvas.renderAll()
+      });
+
+      fabric.Image.fromURL('wp-content/themes/AshleyTLeeTheme/dist/canvas_book.png', function (book) {
+        book.setLeft(125);
+        book.setTop(375);
+        canvas.add(book);
+// eslint-disable-next-line quotes
+        console.log('Added Book');
+        canvas.renderAll()
+      });
+
+      fabric.Image.fromURL('wp-content/themes/AshleyTLeeTheme/dist/canvas_calender.png', function (calender) {
+        calender.setLeft(700);
+        calender.setTop(50);
+        canvas.add(calender);
+// eslint-disable-next-line quotes
+        console.log('Added Calender');
+        canvas.renderAll()
+      });
+
+      fabric.Image.fromURL('wp-content/themes/AshleyTLeeTheme/dist/canvas_holder.png', function (holder) {
+        holder.setLeft(700);
+        holder.setTop(275);
+        canvas.add(holder);
+// eslint-disable-next-line quotes
+        console.log('Added Holder');
+        canvas.renderAll()
+      });
+
+      fabric.Image.fromURL('wp-content/themes/AshleyTLeeTheme/dist/canvas_phone.png', function (phone) {
+        phone.setLeft(850);
+        phone.setTop(300);
+        canvas.add(phone);
+// eslint-disable-next-line quotes
+        console.log('Added Phone');
+        canvas.renderAll()
+      });
+
+      fabric.Image.fromURL('wp-content/themes/AshleyTLeeTheme/dist/canvas_portrait.png', function (portrait) {
+        portrait.setLeft(125);
+        portrait.setTop(50);
+        canvas.add(portrait);
+// eslint-disable-next-line quotes
+        console.log('Added Portrait');
+        canvas.renderAll()
+      });
+
+
+      fabric.Image.fromURL('wp-content/themes/AshleyTLeeTheme/dist/canvas_vase.png', function (vase) {
+        vase.setLeft(250);
+        vase.setTop(240);
+        canvas.add(vase);
+// eslint-disable-next-line quotes
+        console.log('Added Vase');
+        canvas.renderAll()
+      });
+
     },
     name: 'home-canvas',
     data () {
