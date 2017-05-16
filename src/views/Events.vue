7047bff3c51ebc558f74833ef236730ecbe3acb1
adunham95 <Apd@21295>
2017-05-16 13:55:05 -0400

    Started working on calender event

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="template">
     <h1 class="ashley-blue-text">{{title}}</h1>
-
+    <calender-full></calender-full>
   </div>
 </template>
 
 <script>
+    import CalenderFull from "../components/calender/calender.vue";
   export default {
-    name: 'events',
+      components: {CalenderFull},
+      name: 'events',
     data () {
       return {
         title: 'Events'
