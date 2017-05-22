6fea2b369dd7d245211badcd1e91e919c7ee0106
adunham95 <adunham95@gmail.com>
2017-05-22 12:46:16 -0400

    Added prayer-request element

---
+++ src/components/prayer-request..vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="prayer-request">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'prayer-request',
+        data() {
+            return {
+                title: 'prayer-request',
+            };
+        },
+    };
+</script>
+
+<style>
+
+</style>
