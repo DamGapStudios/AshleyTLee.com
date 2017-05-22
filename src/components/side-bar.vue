78613dee85fd4cf90313a54500c94cf96551a2a5
adunham95 <adunham95@gmail.com>
2017-05-21 22:19:17 -0400

    Added sidebar on the blog page

---
+++ src/components/side-bar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="side-bar">
+        <div v-for="blog in post">{{ blog.title.rendered }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'side-bar',
+        props: ['post'],
+        data() {
+            return {
+                title: 'side-bar',
+            };
+        },
+    };
+</script>
+
+<style>
+
+</style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="blog">
     <h1 class="ashley-blue-text"><nav-blog></nav-blog>{{title}}</h1>
-    <blog-post v-for="post in blog_posts" :post="post" class="blogContent"></blog-post>
+    <div class="blogSection">
+      <div class="blogs">
+        <blog-post v-for="post in blog_posts" :post="post" class="blogContent"></blog-post>
+      </div>
+      <side-bar class="sideBar" :post="blog_posts"></side-bar>
+    </div>
   </div>
 </template>
 
 <script>
     import BlogPost from "../components/blogpost.vue";
     import NavBlog from "../components/icons/nav-blog.vue";
+    import SideBar from "../components/side-bar.vue";
   export default {
       components: {
+          SideBar,
           NavBlog,
           BlogPost},
       created () {
@@ -36,7 +43,35 @@
 </style>
 
 <style scoped>
-  .blogContent {
-    padding: 0 5%;
+
+  @media only screen and (min-width : 1024px) {
+   .blogSection{
+     display: flex;
+     padding: 0 5%;
+   }
+
+    .blogs {
+      flex-grow: 2;
+      margin: 0 20px 0 0 ;
+    }
+    .blogContent {
+    }
+
+    .sideBar {
+      width: auto;
+      flex-grow: 1;
+    }
+
+  }
+
+  @media only screen and (max-width : 1024px) {
+    .blogContent {
+      padding: 0 5%;
+    }
+
+    .sideBar {
+      display: none;
+    }
+
   }
 </style>
