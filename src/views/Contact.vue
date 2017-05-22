66beb9485cb2ead1246f124995cf6240de74c665
adunham95 <adunham95@gmail.com>
2017-05-22 12:16:45 -0400

    Added responsiveness to Contact page Issue #30

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -62,127 +62,367 @@
 
 <style scoped>
 
-  #about {
-    display: flex;
-    align-items: center;
-    margin: 30px 5%;
+  @media only screen and (min-width : 1024px) {
+    #about {
+      display: flex;
+      align-items: center;
+      margin: 30px 5%;
+
+    }
+    #promo{
+      background-color: #4B7FC0;
+      color: white;
+      padding: 0 5%;
+    }
+    #contactCard{
+      padding: 0 5%;
+      height: auto;
+      background-color: #8FDCE0;
+      color: white;
+    }
+
+    .text-block {
+      margin: 10px;
+      font-size: 14pt;
+    }
+    .submit{
+      background-color: #5CD1D6;
+      width: 100%;
+      text-align: center;
+      margin: 1px 1px 31px 1px;
+      padding: 10px;
+      border: none;
+      color: white;
+      letter-spacing: normal;
+      word-spacing: normal;
+      text-transform: none;
+      text-indent: 0px;
+      text-shadow: none;
+      display: inline-block;
+      font-family: Helvetica Neue, Helvetica, Arial;
+    }
+
+    .submit:hover{
+      margin: 0 0 30px 0;
+      border: 1px #F7F7F7 solid;
+    }
+    .photo {
+      height: 190px;
+      width: 190px;
+      margin: 10px;
+    }
+
+    .about-button.noside{
+      margin-left: 1px;
+    }
+    .about-button.noside:hover{
+      border: 1px #F7F7F7 solid;
+      margin: 9px 9px 9px 0px;
+    }
+    .about-button{
+      background-color: #5CD1D6;
+      margin: 10px;
+      padding: 5px 10px ;
+      color: white;
+    }
+
+    .about-button:hover{
+      border: 1px #F7F7F7 solid;
+      margin: 9px;
+    }
+    .contact-container{
+      padding: 10px 0 0 0;
+      display: flex;
+      justify-content: space-between;
+    }
+    .input-contact{
+      border: 3px white solid;
+      width: 49%;
+    }
+    input{
+      padding: 10px;
+      -webkit-appearance: textfield;
+      background-color: transparent;
+      -webkit-rtl-ordering: logical;
+      user-select: text;
+      cursor: auto;
+      border: 0;
+      color: white
+
+    }
+    input:focus{
+      outline: none;
+    }
+    textarea {
+      font-family: Helvetica Neue, Helvetica, Arial;
+      border: 3px white solid;
+      width: 100%;
+      margin: 10px 0;
+      padding: 10px;
+      color: white;
+
+      -webkit-appearance: textarea;
+      background-color: transparent;
+      -webkit-rtl-ordering: logical;
+      user-select: text;
+      flex-direction: column;
+      resize: none;
+      cursor: auto;
+      white-space: pre-wrap;
+      word-wrap: break-word;
+    }
+    textarea:focus{
+      outline: none;
+    }
+    ::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+      color: white;
+    }
+    ::-moz-placeholder { /* Firefox 19+ */
+      color: white;
+    }
+    :-ms-input-placeholder { /* IE 10+ */
+      color: white;
+    }
+    :-moz-placeholder { /* Firefox 18- */
+      color: white;
+    }
 
   }
-  #promo{
-    background-color: #4B7FC0;
-    color: white;
-    padding: 0 5%;
-  }
-  #contactCard{
-    padding: 0 5%;
-    height: auto;
-    background-color: #8FDCE0;
-    color: white;
+
+  @media only screen and (max-width : 1023px) and (min-width: 426px) {
+    #about {
+      display: flex;
+      align-items: center;
+      margin: 30px 5%;
+
+    }
+    #promo{
+      background-color: #4B7FC0;
+      color: white;
+      padding: 0 5%;
+    }
+    #contactCard{
+      padding: 0 5%;
+      height: auto;
+      background-color: #8FDCE0;
+      color: white;
+    }
+
+    .text-block {
+      margin: 10px;
+      font-size: 14pt;
+    }
+    .submit{
+      background-color: #5CD1D6;
+      width: 100%;
+      text-align: center;
+      margin: 1px 1px 31px 1px;
+      padding: 10px;
+      border: none;
+      color: white;
+      letter-spacing: normal;
+      word-spacing: normal;
+      text-transform: none;
+      text-indent: 0px;
+      text-shadow: none;
+      display: inline-block;
+      font-family: Helvetica Neue, Helvetica, Arial;
+    }
+
+    .submit:hover{
+      margin: 0 0 30px 0;
+      border: 1px #F7F7F7 solid;
+    }
+    .photo {
+      height: 190px;
+      width: 190px;
+      margin: 10px;
+    }
+
+    .about-button.noside{
+      margin-left: 1px;
+    }
+    .about-button.noside:hover{
+      border: 1px #F7F7F7 solid;
+      margin: 9px 9px 9px 0px;
+    }
+    .about-button{
+      background-color: #5CD1D6;
+      margin: 10px;
+      padding: 5px 10px ;
+      color: white;
+    }
+
+    .about-button:hover{
+      border: 1px #F7F7F7 solid;
+      margin: 9px;
+    }
+    .contact-container{
+      padding: 10px 0 0 0;
+      display: flex;
+      justify-content: space-between;
+    }
+    .input-contact{
+      border: 3px white solid;
+      width: 47%;
+    }
+    input{
+      padding: 10px;
+      -webkit-appearance: textfield;
+      background-color: transparent;
+      -webkit-rtl-ordering: logical;
+      user-select: text;
+      cursor: auto;
+      border: 0;
+      color: white
+
+    }
+    input:focus{
+      outline: none;
+    }
+    textarea {
+      font-family: Helvetica Neue, Helvetica, Arial;
+      border: 3px white solid;
+      width: 100%;
+      margin: 10px 0;
+      padding: 10px;
+      color: white;
+
+      -webkit-appearance: textarea;
+      background-color: transparent;
+      -webkit-rtl-ordering: logical;
+      user-select: text;
+      flex-direction: column;
+      resize: none;
+      cursor: auto;
+      white-space: pre-wrap;
+      word-wrap: break-word;
+    }
+    textarea:focus{
+      outline: none;
+    }
+    ::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+      color: white;
+    }
+    ::-moz-placeholder { /* Firefox 19+ */
+      color: white;
+    }
+    :-ms-input-placeholder { /* IE 10+ */
+      color: white;
+    }
+    :-moz-placeholder { /* Firefox 18- */
+      color: white;
+    }
+
   }
 
-  .text-block {
-    margin: 10px;
-    font-size: 14pt;
-  }
-  .submit{
-    background-color: #5CD1D6;
-    width: 100%;
-    text-align: center;
-    margin: 1px 1px 31px 1px;
-    padding: 10px;
-    border: none;
-    color: white;
-    letter-spacing: normal;
-    word-spacing: normal;
-    text-transform: none;
-    text-indent: 0px;
-    text-shadow: none;
-    display: inline-block;
-    font-family: Helvetica Neue, Helvetica, Arial;
+  @media only screen and (max-width : 425px) {
+
+
+    #promo{
+      background-color: #4B7FC0;
+      color: white;
+      padding: 0 5%;
+      display: flex;
+      flex-direction: column;
+    }
+    #contactCard{
+      padding: 0 5%;
+      height: auto;
+      background-color: #8FDCE0;
+      color: white;
+    }
+
+    .submit{
+      background-color: #5CD1D6;
+      width: 100%;
+      text-align: center;
+      margin: 1px 1px 31px 1px;
+      padding: 10px;
+      border: none;
+      color: white;
+      letter-spacing: normal;
+      word-spacing: normal;
+      text-transform: none;
+      text-indent: 0px;
+      text-shadow: none;
+      display: inline-block;
+      font-family: Helvetica Neue, Helvetica, Arial;
+    }
+
+    .submit:hover{
+      margin: 0 0 30px 0;
+      border: 1px #F7F7F7 solid;
+    }
+
+    .about-button{
+      background-color: #5CD1D6;
+      margin: 10px;
+      padding: 5px 10px ;
+      color: white;
+      width: 95%;
+    }
+
+    .contact-container{
+      padding: 10px 0 0 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+    .input-contact{
+      border: 3px white solid;
+      width: 95%;
+      margin: 10px 0;
+    }
+    input{
+      padding: 10px;
+      -webkit-appearance: textfield;
+      background-color: transparent;
+      -webkit-rtl-ordering: logical;
+      user-select: text;
+      cursor: auto;
+      border: 0;
+      color: white
+
+    }
+    input:focus{
+      outline: none;
+    }
+    textarea {
+      font-family: Helvetica Neue, Helvetica, Arial;
+      border: 3px white solid;
+      width: 90%;
+      margin: 10px 0;
+      padding: 10px;
+      color: white;
+
+      -webkit-appearance: textarea;
+      background-color: transparent;
+      -webkit-rtl-ordering: logical;
+      user-select: text;
+      flex-direction: column;
+      resize: none;
+      cursor: auto;
+      white-space: pre-wrap;
+      word-wrap: break-word;
+    }
+    textarea:focus{
+      outline: none;
+    }
+    ::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+      color: white;
+    }
+    ::-moz-placeholder { /* Firefox 19+ */
+      color: white;
+    }
+    :-ms-input-placeholder { /* IE 10+ */
+      color: white;
+    }
+    :-moz-placeholder { /* Firefox 18- */
+      color: white;
+    }
+
   }
 
-  .submit:hover{
-    margin: 0 0 30px 0;
-    border: 1px #F7F7F7 solid;
-  }
-  .photo {
-    height: 190px;
-    width: 190px;
-    margin: 10px;
-  }
 
-  .about-button.noside{
-    margin-left: 1px;
-  }
-  .about-button.noside:hover{
-    border: 1px #F7F7F7 solid;
-    margin: 9px 9px 9px 0px;
-  }
-  .about-button{
-    background-color: #5CD1D6;
-    margin: 10px;
-    padding: 5px 10px ;
-    color: white;
-  }
-
-  .about-button:hover{
-    border: 1px #F7F7F7 solid;
-    margin: 9px;
-  }
-  .contact-container{
-    padding: 10px 0 0 0;
-    display: flex;
-    justify-content: space-between;
-  }
-  .input-contact{
-    border: 3px white solid;
-    width: 49%;
-  }
-  input{
-    padding: 10px;
-    -webkit-appearance: textfield;
-    background-color: transparent;
-    -webkit-rtl-ordering: logical;
-    user-select: text;
-    cursor: auto;
-    border: 0;
-    color: white
-
-  }
-  input:focus{
-    outline: none;
-  }
-  textarea {
-    font-family: Helvetica Neue, Helvetica, Arial;
-    border: 3px white solid;
-    width: 100%;
-    margin: 10px 0;
-    padding: 10px;
-    color: white;
-
-    -webkit-appearance: textarea;
-    background-color: transparent;
-    -webkit-rtl-ordering: logical;
-    user-select: text;
-    flex-direction: column;
-    resize: none;
-    cursor: auto;
-    white-space: pre-wrap;
-    word-wrap: break-word;
-  }
-  textarea:focus{
-    outline: none;
-  }
-  ::-webkit-input-placeholder { /* Chrome/Opera/Safari */
-    color: white;
-  }
-  ::-moz-placeholder { /* Firefox 19+ */
-    color: white;
-  }
-  :-ms-input-placeholder { /* IE 10+ */
-    color: white;
-  }
-  :-moz-placeholder { /* Firefox 18- */
-    color: white;
-  }
 </style>
