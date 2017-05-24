491f89de61bdfdde8759743677cec8717f5ae8dc
adunham95 <adunham95@gmail.com>
2017-05-24 14:21:37 -0400

    Added event calender

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,23 +1,40 @@
 <template>
   <div id="template">
     <h1 class="ashley-blue-text"><nav-events></nav-events>{{title}}</h1>
-    <calender-full></calender-full>
+    <full-calendar :events="eventSet" locale="en"></full-calendar>
   </div>
 </template>
 
 <script>
     import CalenderFull from "../components/calender/calender.vue";
     import NavEvents from "../components/icons/nav-events.vue";
+
   export default {
       components: {
           NavEvents,
-          CalenderFull},
+          CalenderFull,
+          'full-calendar': require('vue-fullcalendar'),
+      },
       name: 'events',
     data () {
       return {
-        title: 'Events'
+        title: 'Events',
+          events:[],
+          eventSet: []
       }
-    }
+    },
+      created(){
+          this.$http.get('http://ashleytlee.dev/wp-json/tribe/events/v1/events').then(response => {
+              this.events = response.body.events;
+              console.log(this.events);
+              for (let i = 0; i < this.events.length; i++) {
+                  console.log({title: this.events[i].title, start: this.events[i].start_date, end: this.events[i].end_date});
+                  this.eventSet.push({title: this.events[i].title, start: this.events[i].start_date, end: this.events[i].end_date});
+                  console.log(this.eventSet)
+              }
+          }, response => {
+          })
+      }
   }
 </script>
 
