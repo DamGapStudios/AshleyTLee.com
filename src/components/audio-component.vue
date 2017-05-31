7ffff7ee3e0daeb598569a9e39643b4d278e63fe
adunham95 <adunham95@gmail.com>
2017-05-31 16:46:15 -0400

    Working on images in message page Issue #35

--- src/components/audio-component.vue
+++ src/components/audio-component.vue
@@ -1,11 +1,10 @@
 <template>
     <div id="audio-component">
-        <div class="audio-image" :style="{ backgroundImage: 'url(' + image + ')' }">
-            <audio controls style="width: 200px; margin-top: 168px">
+        <img class="audio-image" :src.sync="this.image" v-if="loaded">
+            <audio controls style="width: 200px;">
                 <source :src="audio.sermon_audio" type="audio/mpeg">
                 Your browser does not support the audio element
             </audio>
-        </div>
         <div v-html="audio.title.rendered"></div><br/>
         <span style="font-size: 12px">{{ audio.date }}</span>
     </div>
@@ -17,22 +16,21 @@
         data() {
             return {
                 title: 'audio-component',
+                loaded: false
         };
         },
-        props: ['audio'],
-        computed: {
-            image: function () {
-                this.$http.get('http://ashleytlee.com/wp-json/wp/v2/media/' + this.audio.featured_media).then(response => {
-                    return response.body.guid.rendered;
-                })
-            }
+        props: ['audio', 'images'],
+        created() {
+            console.log(this.audio.id, this.images);
         },
     };
 </script>
 
 <style scoped>
     .audio-image{
+        position: relative;
         width: 200px;
         height: 200px;
+        overflow: hidden;
     }
 </style>
