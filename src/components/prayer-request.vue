6e4cbefa7c8a4d78933e6214f9af403f77bbbf3a
adunham95 <adunham95@gmail.com>
2017-05-31 14:12:04 -0400

    Added prayer request component Issue #35

--- src/components/prayer-request.vue
+++ src/components/prayer-request.vue
@@ -1,12 +1,19 @@
 <template>
     <div id="prayer-request">
-        BNEW: Prayer Request Book - by Ashley T Lee! Available now on Amazon.
+        <div class="text">NEW: Prayer Request Book - by Ashley T Lee! Available now on Amazon.</div>
         <a style="font-size: x-large" href="" class="about-button">See More</a>
+        <prayer-request-book class="book"></prayer-request-book>
+        <prayer-request-text class="styledText"></prayer-request-text>
     </div>
 </template>
 
 <script>
+    import PrayerRequestBook from "./prayerRequest-book.vue";
+    import PrayerRequestText from "./prayerRequest-text.vue";
     export default {
+        components: {
+            PrayerRequestText,
+            PrayerRequestBook},
         name: 'prayer-request',
         data() {
             return {
@@ -29,7 +36,54 @@
          font-size: 30px;
          background-color: #4B7FC0;
          color: white;
-         padding: 100px 5%;
+         padding: 20px 5% 0;
+         display: flex;
+         flex-direction: row;
+         align-items: center;
+     }
+
+     .about-button{
+         padding: 11px 10px ;
+         color: white;
+         background-color: #5CD1D6;
+         margin: 10px;
+         text-align: center;
+         font-size: 14pt;
+         flex-basis: 150px;
+     }
+
+     .book {
+         flex-basis: 15%;
+     }
+
+     .styledText {
+         flex-basis: 20%;
+         fill:#FFFFFF;
+         font-family:'Monthoers-Vintage';
+         font-size:49px;
+     }
+
+     .text {
+     }
+ }
+
+ @media only screen and (max-width : 1159px) and (min-width: 426px) {
+     #prayer-request{
+         text-align: center;
+         font-size: 30px;
+         background-color: #4B7FC0;
+         color: white;
+         padding: 20px 5%;
+         display: flex;
+         flex-direction: column;
+     }
+
+     .about-button{
+         color: white;
+         padding: 10px 11px;
+         background-color: #5CD1D6;
+         margin: 10px;
+         text-align: center;
      }
 
      .about-button{
@@ -40,25 +94,20 @@
          text-align: center;
          font-size: 14pt;
      }
- }
 
- @media only screen and (max-width : 1159px) and (min-width: 426px) {
-     #prayer-request{
-         text-align: center;
-         font-size: 30px;
-         background-color: #4B7FC0;
-         color: white;
-         padding: 100px 5%;
-         display: flex;
-         flex-direction: column;
+     .book {
+         flex-basis: 15%;
      }
 
-     .about-button{
-         color: white;
-         padding: 10px 11px;
-         background-color: #5CD1D6;
-         margin: 10px;
-         text-align: center;
+     .styledText {
+         flex-basis: 20%;
+         fill:#FFFFFF;
+         font-family:'Monthoers-Vintage';
+         font-size:49px;
+     }
+
+     .text {
+         flex-basis: 25%;
      }
  }
 
@@ -68,7 +117,7 @@
          font-size: 30px;
          background-color: #4B7FC0;
          color: white;
-         padding: 100px 5%;
+         padding: 20px 5%;
          display: flex;
          flex-direction: column;
 
@@ -80,6 +129,30 @@
          background-color: #5CD1D6;
          margin: 10px;
          text-align: center;
+     }
+
+     .about-button{
+         padding: 11px 10px ;
+         color: white;
+         background-color: #5CD1D6;
+         margin: 10px;
+         text-align: center;
+         font-size: 14pt;
+     }
+
+     .book {
+         flex-basis: 15%;
+     }
+
+     .styledText {
+         flex-basis: 20%;
+         fill:#FFFFFF;
+         font-family:'Monthoers-Vintage';
+         font-size:49px;
+     }
+
+     .text {
+         flex-basis: 25%;
      }
  }
 </style>
---
+++ src/components/prayerRequest-text.vue
@@ -0,0 +1,23 @@
+<template>
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 267.1 265.7" style="enable-background:new 0 0 267.1 265.7;" xml:space="preserve">
+<text transform="matrix(1 0 0 1 113.161 215.486)"><tspan x="0" y="0" class="st0 st1 st2">Book</tspan><tspan x="91.9" y="0" class="st0 st1 st3 st4">    </tspan><tspan x="164.6" y="0" class="st0 st1 st5 st4">   </tspan></text>
+<text transform="matrix(1 0 0 1 25.9869 114.3132)" class="st0 st1 st6">Prayer</text>
+<text transform="matrix(1 0 0 1 56.5299 162.1226)" class="st0 st1 st6">Request</text>
+</svg>
+</template>
+
+<script>
+    export default {
+        name: 'prayerRequest-text',
+        data() {
+            return {
+                title: 'prayerRequest-text',
+            };
+        },
+    };
+</script>
+
+<style>
+
+</style>
