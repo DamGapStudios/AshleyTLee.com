88877f90c795b4773424ae02b81778fcaf0498df
adunham95 <adunham95@gmail.com>
2017-06-01 12:10:09 -0400

    Changed from .dev to .com

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,6 +1,22 @@
 <template>
-  <div id="blog" class="top-more">
-    <div class="ashley-blue-text"><nav-blog class="body-icon"></nav-blog><blog-title class="body-icon"></blog-title></div><br/>
+  <div id="blog">
+    <div class="header">
+      <blog-title class="header title"></blog-title>
+
+      <div class="header box-container">
+
+        <div class="header box">
+
+        </div>
+
+        <div class="header portrait">
+          <img src="wp-content/themes/AshleyTLeeTheme/dist/ashley_portrait.png" class="logo"/>
+        </div>
+
+      </div>
+
+    </div>
+
     <div class="blogSection">
       <div class="blogs">
         <blog-post v-for="post in blog_posts" :post="post" class="blogContent"></blog-post>
@@ -22,7 +38,7 @@
           NavBlog,
           BlogPost},
       created () {
-      this.$http.get('http://ashleytlee.dev/wp-json/wp/v2/posts').then(response => {
+      this.$http.get('http://ashleytlee.com/wp-json/wp/v2/posts').then(response => {
         this.blog_posts = response.body;
 //        console.log(this.blog_posts)
       }, response => {})
@@ -46,6 +62,40 @@
   }
 
   @media only screen and (min-width : 1160px) {
+    .header {
+      background: #56D1D7;
+      padding: 10px 5% 20px ;
+    }
+
+    .header.title{
+      display: block;
+      margin: auto;
+      height: 90px;
+      fill: white;
+      border: white 5px solid;
+      padding: 20px 30px;
+    }
+
+    .header.box-container{
+      display: flex;
+      padding: 0;
+      flex-direction: row;
+    }
+
+    .header.box{
+      border: outset white 10px;
+      flex-basis: 90%;
+      margin: 10px;
+    }
+
+    .header.portrait{
+      border: outset white 10px;
+      flex-basis: 252px;
+      height: 252px;
+      margin: 10px;
+      padding: 0;
+    }
+
    .blogSection{
      display: inline-flex;
      flex-direction: row;
