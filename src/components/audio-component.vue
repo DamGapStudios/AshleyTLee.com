f52b8487eb813fbf0e6587f56c988cf7276dc600
adunham95 <adunham95@gmail.com>
2017-06-01 12:08:27 -0400

    Added images on messaging and app page Issue #48 Issue #51

--- src/components/audio-component.vue
+++ src/components/audio-component.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="audio-component">
-        <img class="audio-image" :src="images">
+        <img class="audio-image" :src.sync="images" v-if="loaded">
+        <div class="background" v-if="loading">Loading....</div>
             <audio controls style="width: 200px;">
                 <source :src="audio.sermon_audio" type="audio/mpeg">
                 Your browser does not support the audio element
@@ -16,19 +17,47 @@
         data() {
             return {
                 title: 'audio-component',
-                loaded: false
+                loaded: false,
+                loading: true,
         };
         },
-        props: ['audio', 'images']
+        props: ['audio', 'images'],
+        created() {
+            console.log(this.audio.featured_media);
+
+            if(isNaN(this.audio.featured_media)){
+                console.log("Not number");
+                this.loaded = true;
+                this.loading = false
+            }
+            else if (this.audio.featured_media === 0) {
+                this.images = 'http://www.ashleytlee.com/wp-content/uploads/2017/06/ashley_portrait.png';
+                console.log("YES");
+                this.loaded = true;
+                this.loading = false
+            }
+            else {
+                console.log("NO");
+                this.$http.get('http://ashleytlee.com/wp-json/wp/v2/media/' + this.audio.featured_media).then(response => {
+                    this.images = response.body.media_details.sizes.thumbnail.source_url;
+                    this.loaded = true;
+                    this.loading = false
+                })
+            }
+        }
     };
 </script>
 
 <style scoped>
     .audio-image{
-        position: relative;
         width: 200px;
         height: 200px;
         overflow: hidden;
-        background: #4B7FC0;
+        background: transparent;
+    }
+    .background {
+        width: 200px;
+        height: 200px;
+        background: #a3e1e5;
     }
 </style>
